b70c84374363abef91c7e5b2447870dd140bede6
defaulterrr <sp@zero.ru>
2019-12-08 01:03:12 +0300

    merge fix

---
+++ app/lib.hpp
@@ -0,0 +1,7 @@
+#include <iostream>
+using namespace std;
+#include "../models/h_HashTable_one.hpp"
+#include "../models/h_HashTable_two.hpp"
+#include "../models/h_HashTable_three.hpp"
+#include "../models/h_Person.hpp"
+#include "Command.h"
